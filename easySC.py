70db1561439cf5b336fbe544010b7cb7bbbe8457
ricklin <yiilih@gmail.com>
2023-03-01 21:36:09 +0100

    input argument

--- easySC.py
+++ easySC.py
@@ -1,27 +1,68 @@
 ## library
+import argparse
+from pathlib import Path
 
 ## functions
-
 def get_input():
-    ## ToDO: read data and check input
+    '''
+    Read data and check input
+    https://docs.python.org/3/library/argparse.html
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data','-d', 
+                        required=True,
+                        type=Path)
+    p = parser.parse_args()
+
+    p.data = p.data.resolve() # full file path
+    if p.data.is_dir():
+        print(f"Input data dir: {p.data}")
+    else:
+        print(f"Does not exist: {p.data}")
+    
+    files = ["barcodes.tsv.gz", "features.tsv.gz", "matrix.mtx.gz"]
+    paths = list()
+    for f in files:
+        f = p.data / f
+        if f.is_file():
+            paths.append(f)
+        else:
+            print(f"missing file: {f}")
+
+    return paths
+
+def load_data(paths):
+    '''
+    Load the 3 data files
+    Save to a data class object
+    '''
+    print(paths)
 
 def filter_data():
-    ## ToDo: perform basic filtering, umi/gene, gene/cell
+    '''ToDo: perform basic filtering, umi/gene, gene/cell'''
+    pass
 
 def make_analysis():
-    ## generate analysis and plot
+    '''generate analysis and plot'''
+    pass
 
 def save_csv():
-    ## ToDo: write results into csv
+    '''ToDo: write results into csv'''
+    pass
 
 def save_plot():
-    ## save plots
+    '''save plots'''
+    pass
 
 
 ## main
-input = get_input()
-filtered = filter_data(input)
-make_analysis(filtered)
+if __name__ == "__main__":
 
-save_csv()
-save_plot()
+    paths = get_input()
+
+    load_data(paths)
+    filter_data()
+    make_analysis()
+
+    save_csv()
+    save_plot()
